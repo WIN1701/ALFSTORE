0d7e12652df692d94eeae9582220396bb91e81df
WIN1701 <hernandezmartiredwinsteven@gmail.com>
2026-09-03 21:37:31 -0600

    Corregir configuracion para Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/ALFSTORE_1",
-  assetPrefix: "/ALFSTORE_1/",
   images: {
     unoptimized: true,
   },
